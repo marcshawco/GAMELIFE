11528181c143e15ad547a2282b9480a8f349541b
Marcus <marcus@marcuslshaw.com>
2026-02-05 04:33:03 -0800

    Initial Commit

---
+++ GAMELIFE/GAMELIFEApp.swift
@@ -0,0 +1,17 @@
+//
+//  GAMELIFEApp.swift
+//  GAMELIFE
+//
+//  Created by Marcus Shaw II on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct GAMELIFEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
